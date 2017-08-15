0bc72de868d90ba52edf218381588f190ba47927
hansen0086 <hansen0086@163.com>
2017-08-15 15:39:02 +1000

    Add files via upload

---
+++ lab04/csv_fn.py
@@ -0,0 +1,12 @@
+import csv
+def write_to_csv(filename,data):
+    with open('example.csv','a') as csv_out:
+        writer = csv.writer(csv_out)
+        writer.writerow(data)
+    
+def print_from_csv(filename):
+   with open('example.csv','r') as csv_in:
+        reader = csv.reader(csv_in)
+        for row in reader:
+            print(row)
+    
---
+++ lab04/routes.py
@@ -0,0 +1,25 @@
+from flask import Flask, redirect, render_template, request, url_for
+from server import app, user_input
+import csv
+from csv_fn import *
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        zID = int(request.form["zID"])
+        description = request.form["desc"]
+        user_input.append([name, zID, description])
+        #with open('example','a') as csv_out:
+             #writer = csv.writer(csv_out)
+             #writer.writerow([name, zID, description])
+        write_to_csv("example.csv",[name, zID, description])
+         
+        return redirect(url_for("hello"))
+    return render_template("index.html")
+
+@app.route("/Hello")
+def hello():
+    print_from_csv("example.csv")
+    
+    return render_template("hello.html", all_users=user_input) 
+
